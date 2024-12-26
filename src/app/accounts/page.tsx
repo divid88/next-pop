778312a707e8366283456df18ab68ad0ad9aa7cf
david <divid28p@gmail.com>
2024-12-26 14:29:54 +0400

    refix accounts

--- src/app/accounts/page.tsx
+++ src/app/accounts/page.tsx
@@ -18,7 +18,7 @@ async function accounts() {
   return (
 
     <div className="flex flex-col items-center justify-center h-full mt-10">
-    <Tabs defaultValue="account" className="w-[400px]">
+    <Tabs defaultValue="account" className=" xs:w-[370px]  md:w-[400px]">
       <TabsList className="grid w-full grid-cols-2">
         <TabsTrigger value="account"> ثبت نام </TabsTrigger>
         <TabsTrigger value="password"> ورود </TabsTrigger>
